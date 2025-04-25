509c9a44ee039d6a9c8b6783fca63e11f555274a
Sabina Sultanova <sultanovasabina89@gmail.com>
2025-04-25 08:43:18 +0300

    added new logic

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {System.out.println("Введите текст и нажмите <Enter>:");
-            String text = new Scanner(System.in) .nextLine();
-            System.out.println("Длина текста: " + text.length());
+    public static void main(String[] args) {System.out.println("Введите число:");
+            int number = new Scanner(System.in) .nextInt();
+            int number2 = new Scanner(System.in) .nextInt();
+
+            int firstNumber = 5;
+            int secondNumber = 10;
+            int sum=firstNumber+secondNumber;
+            double quotient = (double) firstNumber / secondNumber;
+            System.out.println(sum);
+            System.out.println(firstNumber-secondNumber);
+            System.out.println(firstNumber*secondNumber);
+            System.out.println(quotient);
         }
     }
