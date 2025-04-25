51bf0fa2c324e32204f53eceffc8bc2692b61029
Sabina Sultanova <sultanovasabina89@gmail.com>
2025-04-25 09:01:44 +0300

    changed new logic

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {System.out.println("Введите число:");
+    public static void main(String[] args) {System.out.println("Введите первое число:");
             int number = new Scanner(System.in) .nextInt();
+    System.out.println("Введите второе число:");
             int number2 = new Scanner(System.in) .nextInt();
 
-            int firstNumber = 5;
-            int secondNumber = 10;
+            int firstNumber = number;
+            int secondNumber = number2;
             int sum=firstNumber+secondNumber;
             double quotient = (double) firstNumber / secondNumber;
             System.out.println(sum);
