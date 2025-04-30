b96c61939702b8e889375516a63fa8838dc9cb4a
Sabina Sultanova <sultanovasabina89@gmail.com>
2025-04-30 06:53:56 +0300

    changed variables

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {System.out.println("Введите первое число:");
-            int number = new Scanner(System.in) .nextInt();
-    System.out.println("Введите второе число:");
-            int number2 = new Scanner(System.in) .nextInt();
+    public static void main(String[] args) {
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
 
-            int firstNumber = number;
-            int secondNumber = number2;
-            int sum=firstNumber+secondNumber;
-            double quotient = (double) firstNumber / secondNumber;
-            System.out.println("Сумма:"+sum);
-            System.out.println("Разность:"+(firstNumber-secondNumber));
-            System.out.println("Произведение:"+(firstNumber*secondNumber));
-            System.out.println("Частное:"+quotient);
-        }
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма:" + (firstNumber + secondNumber));
+        System.out.println("Разность:" + (firstNumber - secondNumber));
+        System.out.println("Произведение:" + (firstNumber * secondNumber));
+        System.out.println("Частное:" + quotient);
     }
+}
